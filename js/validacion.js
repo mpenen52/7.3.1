c0b14eab6d60aa745bea02ed1582be21e7a75986
florencia menendez <flor_mm10@hotmail.com>
2023-08-10 19:12:15 -0300

    campos resueltos

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,22 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const form = document.querySelector("form-label");
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const email = document.getElementById("email");
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+const button = document.getElementById('regBtn');
+
+const para = document.querySelector("alert-danger");
+button.addEventListener("click", (e) => {
+  if (nombre.value === "" || apellido.value === "" || email.value === "" || password1.value === "" || password2.value === "" && terminos.value === false) {
+    e.preventDefault();
+    showAlertError();
+  } else {
+    showAlertSuccess();
+  }
+});
+
